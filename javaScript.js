b2f41f6be532ded598c767f674f0aa06491d0e66
Ian Cowan <ian@cowanemail.com>
2020-03-12 15:01:37 -0400

    Finish non-table faculty

--- javaScript.js
+++ javaScript.js
@@ -1,6 +1,11 @@
 var facultyPromise = d3.json("employee.json")
 facultyPromise.then(
-function(employees){facultyTable(employees)},function(err){console.log("failed:",err)});
+function(employees){createTables(employees)},function(err){console.log("failed:",err)});
+
+var createTables = function(employees) {
+    facultyTable(employees);
+    facultyNonTable(employees);
+}
 
 var facultyTable = function(employees)
 {
@@ -13,4 +18,47 @@ var facultyTable = function(employees)
     rows.append("td")
     .text(function(employee){return employee.firstName})
     
+    rows.append("td")
+    .text(function(employee){return employee.lastName})
+    
+    rows.append("td")
+    .append("img")
+    .attr("src",function(employee){return employee.photo})
+    
+    rows.append("td")
+    .text(function(employee){return employee.title})
+    
+    rows.append("td")
+    .text(function(employee){return employee.unit})
+    
+    rows.append("td")
+    .text(function(employee){return employee.email})
+    
+    rows.append("td")
+    .text(function(employee){return employee.bio})
+    
+    rows.append("td")
+    .text(function(employee){return employee.phone})
+}
+
+var facultyNonTable = function(employees) {
+    var cards = d3.select("#employeeCards")
+      .selectAll("div")
+      .data(employees)
+      .enter()
+      .append("div").classed("employee", true);
+    
+    employeeHead = cards.append("div")
+      .classed("general", true);
+    
+    employeeHead.append("span").classed("eName", true).text(function(employee){return employee.firstName + " " + employee.lastName});
+    employeeHead.append("img").attr("src", function(employee){return employee.photo});
+    
+    employeeDetail = cards.append("div")
+      .classed("detail", true);
+    
+    employeeDetail.append("span").classed("title", true).text(function(employee){return employee.title});
+    employeeDetail.append("span").classed("department", true).text(function(employee){return employee.unit});
+    employeeDetail.append("span").classed("email", true).text(function(employee){return employee.email});
+    employeeDetail.append("p").classed("bio", true).text(function(employee){return employee.bio});
 }
